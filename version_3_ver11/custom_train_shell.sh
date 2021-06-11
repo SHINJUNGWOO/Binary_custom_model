1ccc46549cf0719a8b969c5cef05d428c07d75da
SHINJUNGWOO <sjo506@naver.com>
2021-06-12 00:56:11 +0900

    Local LN X

---
+++ version_3_ver11/custom_train_shell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SAVEDIR='./propose_ptr/version_1'
+TRAINNAME="custom_train_2stage.py"
+Epoch=400
+
+#: <<"END"
+python -u ${TRAINNAME} \
+    --weight_decay 1e-5 \
+    --epochs ${Epoch} \
+    --save ${SAVEDIR}/pre
+
+#END
+
+python -u ${TRAINNAME} \
+    --weight_decay 0 \
+    --epochs ${Epoch} \
+    --save ${SAVEDIR}/post\
+    --binary_w \
+    --pretrained ${SAVEDIR}/pre/model_best.pth.tar \
+
+
